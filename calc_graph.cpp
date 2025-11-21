7dbfbe32542986f807173be518b76a2fb510bc58
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-21 21:27:02 +0200

    feat: implement slow and quick computations

--- calc_graph.cpp
+++ calc_graph.cpp
@@ -1,14 +1,32 @@
 #include "calc_graph.h"
 
+#include <chrono>
+#include <iostream>
+#include <syncstream>
+#include <thread>
+
 namespace calc_graph {
+    static void run_step(const std::string& name, int seconds) {
+        std::this_thread::sleep_for(std::chrono::seconds(seconds));
+        std::osyncstream(std::cout) << name << '\n';
+    }
+
     void slow(const std::string& name) {
-        (void)name;
+        run_step(name, 7);
     }
 
     void quick(const std::string& name) {
-        (void)name;
+        run_step(name, 1);
     }
 
     void work() {
+        slow("A");
+        quick("B");
+        slow("C");
+
+        quick("D1");
+        quick("D2");
+        quick("F1");
+        quick("F2");
     }
 }
